63fe15a5bae20fdd5cc2fd15b1588552fa40a3fe
Moses Johansson <mosesjohansson@gmail.com>
2018-03-23 21:06:04 +0100

    another component for testing, for loop with dummy data

---
+++ client/src/components/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout>
+    <v-flex xs6 offset-xs3>
+      <panel title="Home">
+          <p>We have content!</p>
+      </panel>
+      <PanelTest thetitle="Testing..">
+          <p>We have content here as well!</p>
+      </PanelTest>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Panel from '@/components/Panel'
+import PanelTest from '@/components/PanelTest'
+
+export default {
+  components: {
+    Panel,
+    PanelTest
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error {
+  color: red;
+}
+</style>
--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,6 +2,12 @@
     <v-layout>
         <v-flex xs6 offset-xs3>
             <panel title="Songs">
+              <div v-for="song in songs"
+                :key="song.title">
+                {{song.title}}
+                {{song.artist}}
+                {{song.album}}
+              </div>
             </panel>
         </v-flex>
     </v-layout>
@@ -12,6 +18,17 @@ import Panel from '@/components/Panel'
 export default {
   components: {
     Panel
+  },
+  data () {
+    return {
+      songs: [
+        {
+          title: 'Music Title',
+          artist: 'Underoath',
+          album: 'A boy brushed in red..'
+        }
+      ]
+    }
   }
 }
 </script>
