058254ed8ce308a67fd6322c13f1ee18522e03e7
Moses Johansson <mosesjohansson@gmail.com>
2018-03-29 22:20:24 +0200

    view single song component

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,20 +1,30 @@
 <template>
-<div></div>
+<v-layout>
+  <v-flex xs4>
+    <panel title="Song metadata">
+       {{song.title}}
+    </panel>
+  </v-flex>
+</v-layout>
 </template>
 
 <script>
 import SongsService from '@/services/SongsService'
+import Panel from '@/components/Panel'
+
 
 export default {
   data () {
     return {
-      song: null
+      song: {}
     }
+  },
+  components: {
+    Panel
   },
   async mounted () {
     const songId = this.$store.state.route.params.songId
-    const song = await SongsService.show(songId)
-    console.log(song)
+    this.song = (await SongsService.show(songId)).data
   }
 }
 </script>
